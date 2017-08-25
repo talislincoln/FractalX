07ca21513d1a81ed28d958a930267b5092edc803
Talis Lincoln <talislincoln@gmail.com>
2017-08-25 08:39:13 -0400

    basic opening window

--- FractalX/WinMain.cpp
+++ FractalX/WinMain.cpp
@@ -1,5 +1,98 @@
 #include <FractalPCH.h>
 #include "WinMain.h"
+#include <algorithm>
+
+const LONG g_WindowWidth = 1280;
+const LONG g_WindowHeight = 720;
+LPCSTR g_WindowClassName = "DirectXWindowClass";
+LPCSTR g_WindowName = "DirectX Template";
+HWND g_WindowHandle = 0;
+
+const BOOL g_EnableVSync = TRUE;
+
+// Direct3D device and swap chain.
+ID3D11Device* g_d3dDevice = nullptr;
+ID3D11DeviceContext* g_d3dDeviceContext = nullptr;
+IDXGISwapChain* g_d3dSwapChain = nullptr;// Vertex buffer data
+ID3D11InputLayout* g_d3dInputLayout = nullptr;
+ID3D11Buffer* g_d3dVertexBuffer = nullptr;
+ID3D11Buffer* g_d3dIndexBuffer = nullptr;
+
+// Shader data
+ID3D11VertexShader* g_d3dVertexShader = nullptr;
+ID3D11PixelShader* g_d3dPixelShader = nullptr;
+
+// Render target view for the back buffer of the swap chain.
+ID3D11RenderTargetView* g_d3dRenderTargetView = nullptr;
+// Depth/stencil view for use as a depth buffer.
+ID3D11DepthStencilView* g_d3dDepthStencilView = nullptr;
+// A texture to associate to the depth stencil view.
+ID3D11Texture2D* g_d3dDepthStencilBuffer = nullptr;
+
+// Define the functionality of the depth/stencil stages.
+ID3D11DepthStencilState* g_d3dDepthStencilState = nullptr;
+// Define the functionality of the rasterizer stage.
+ID3D11RasterizerState* g_d3dRasterizerState = nullptr;
+D3D11_VIEWPORT g_Viewport = { 0 };
+
+// Demo parameters
+DirectX::XMMATRIX g_WorldMatrix;
+DirectX::XMMATRIX g_ViewMatrix;
+DirectX::XMMATRIX g_ProjectionMatrix;
+
+// Shader resources
+enum ConstanBuffer
+{
+	CB_Appliation,
+	CB_Frame,
+	CB_Object,
+	NumConstantBuffers
+};
+
+ID3D11Buffer* g_d3dConstantBuffers[NumConstantBuffers];
+
+// Vertex data for a colored cube.
+struct VertexPosColor
+{
+	DirectX::XMFLOAT3 Position;
+	DirectX::XMFLOAT3 Color;
+};
+
+VertexPosColor g_Vertices[8] =
+{
+	{ DirectX::XMFLOAT3(-1.0f, -1.0f, -1.0f), DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f) }, // 0
+	{ DirectX::XMFLOAT3(-1.0f,  1.0f, -1.0f), DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f) }, // 1
+	{ DirectX::XMFLOAT3(1.0f,  1.0f, -1.0f),  DirectX::XMFLOAT3(1.0f, 1.0f, 0.0f) }, // 2
+	{ DirectX::XMFLOAT3(1.0f, -1.0f, -1.0f),  DirectX::XMFLOAT3(1.0f, 0.0f, 0.0f) }, // 3
+	{ DirectX::XMFLOAT3(-1.0f, -1.0f,  1.0f), DirectX::XMFLOAT3(0.0f, 0.0f, 1.0f) }, // 4
+	{ DirectX::XMFLOAT3(-1.0f,  1.0f,  1.0f), DirectX::XMFLOAT3(0.0f, 1.0f, 1.0f) }, // 5
+	{ DirectX::XMFLOAT3(1.0f,  1.0f,  1.0f),  DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f) }, // 6
+	{ DirectX::XMFLOAT3(1.0f, -1.0f,  1.0f),  DirectX::XMFLOAT3(1.0f, 0.0f, 1.0f) }  // 7
+};
+
+WORD g_Indicies[36] =
+{
+	0, 1, 2, 0, 2, 3,
+	4, 6, 5, 4, 7, 6,
+	4, 5, 1, 4, 1, 0,
+	3, 2, 6, 3, 6, 7,
+	1, 5, 6, 1, 6, 2,
+	4, 0, 3, 4, 3, 7
+};
+
+// Forward declarations.
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+template< class ShaderClass >
+ShaderClass* LoadShader(const std::wstring& fileName, const std::string& entryPoint, const std::string& profile);
+
+bool LoadContent();
+void UnloadContent();
+int  Run();
+void Update(float deltaTime);
+void Render();
+void Cleanup();
+int InitApplication(HINSTANCE hInstance, int cmdShow);
 
 #if defined (DEBUG) | defined (_DEBUG)
 int _tmain(int argc, char* argv[])
@@ -15,10 +108,24 @@ int _tmain(int argc, char* argv[])
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hInstance);
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
-	UNREFERENCED_PARAMETER(nCmdShow);
+
+	// Check for DirectX Math library support.
+	if (!DirectX::XMVerifyCPUSupport())
+	{
+		MessageBox(nullptr, TEXT("Failed to verify DirectX Math library support."), TEXT("Error"), MB_OK);
+		return -1;
+	}
+
+	if (InitApplication(hInstance, nCmdShow) != 0)
+	{
+		MessageBox(nullptr, TEXT("Failed to create applicaiton window."), TEXT("Error"), MB_OK);
+		return -1;
+	}
+
+	int returnCode = Run();
+
 
 #if defined (DEBUG) | defined (_DEBUG)
 	HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
@@ -30,5 +137,103 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	//Create the engine
 
+	return returnCode;
+}
+
+/**
+* Initialize the application window.
+*/
+int InitApplication(HINSTANCE hInstance, int cmdShow)
+{
+	WNDCLASSEX wndClass = { 0 };
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = &WndProc;
+	wndClass.hInstance = hInstance;
+	wndClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClass.lpszMenuName = nullptr;
+	wndClass.lpszClassName = _TEXT("DirectXWindowClass"); // should be using the global variable, but I can't found a solution for the error
+
+	if (!RegisterClassEx(&wndClass))
+	{
+		return -1;
+	}
+
+	RECT windowRect = { 0, 0, g_WindowWidth, g_WindowHeight };
+	AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	g_WindowHandle = CreateWindowA (g_WindowClassName, g_WindowName,
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		windowRect.right - windowRect.left,
+		windowRect.bottom - windowRect.top,
+		nullptr, nullptr, hInstance, nullptr);
+
+	if (!g_WindowHandle)
+	{
+		return -1;
+	}
+
+	ShowWindow(g_WindowHandle, cmdShow);
+	UpdateWindow(g_WindowHandle);
+
 	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT paintStruct;
+	HDC hDC;
+
+	switch (message)
+	{
+	case WM_PAINT:
+	{
+		hDC = BeginPaint(hwnd, &paintStruct);
+		EndPaint(hwnd, &paintStruct);
+	}
+	break;
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+	}
+	break;
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+int Run()
+{
+	MSG msg = { 0 };
+
+	static DWORD previousTime = timeGetTime();
+	static const float targetFramerate = 30.0f;
+	static const float maxTimeStep = 1.0f / targetFramerate;
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			DWORD currentTime = timeGetTime();
+			float deltaTime = (currentTime - previousTime) / 1000.0f;
+			previousTime = currentTime;
+
+			// Cap the delta time to the max time step (useful if your 
+			// debugging and you don't want the deltaTime value to explode.
+			deltaTime = std::min<float>(deltaTime, maxTimeStep);
+
+			//            Update( deltaTime );
+			//            Render();
+		}
+	}
+
+	return static_cast<int>(msg.wParam);
 }
