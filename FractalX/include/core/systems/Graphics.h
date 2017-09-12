da5465dfabbf18687ba408731859e76006003bad
Talis Lincoln <talislincoln@gmail.com>
2017-09-12 01:51:52 -0400

    started the graphics system

---
+++ FractalX/include/core/systems/Graphics.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef _GRAPHICS_H
+#define _GRAPHICS_H
+#include "System.h"
+namespace fractal
+{
+	namespace fcore
+	{
+		class Graphics : public System
+		{
+		public:
+			Graphics ();
+			~Graphics ();
+		};
+	}
+}
+#endif // !_GRAPHICS_H
---
+++ FractalX/src/core/systems/Graphics.cpp
@@ -0,0 +1,19 @@
+#include <FractalPCH.h>
+#include <core\systems\Graphics.h>
+
+namespace fractal
+{
+	namespace fcore
+	{
+		Graphics::Graphics () :
+			System (SystemType::GRAPHICS_SYSTEM)
+		{
+
+		}
+
+		Graphics::~Graphics ()
+		{
+
+		}
+	}
+}
